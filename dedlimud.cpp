6842cef63dc8c024ed2a8f2f0a08aaba869e0430
Romyl4 <153442189+Romyl4@users.noreply.github.com>
2024-03-04 20:54:28 +0200

    Add files via upload

---
+++ dedlimud.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int h;
+    cout << "Введіть висоту піраміди: ";
+    cin >> h;
+
+    for (int i = 1; i <= h; ++i) {
+        for (int j = 1; j <= h - i; ++j) {
+            cout << " ";
+
+        }
+
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+
+        for (int j = i + 1; j <= 2 * i - 1; ++j) {
+            cout << i - 1;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
