7fd6c8bd6aa8eb5bdf54e19221ec9880f1d10ba6
Romyl4 <153442189+Romyl4@users.noreply.github.com>
2024-03-26 12:29:35 +0200

    Add files via upload
    Лабораторна 2. Шаблонна і звичайна функції.

---
+++ lab2_F1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int factorial(int n) {
+    int result = 1;
+    for (int j = 1; j <= n; ++j) {
+        result *= j;
+    }
+    return result;
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int n, factorial_n, factorial_neg_n;
+
+    cout << "Введіть n: ";
+    cin >> n;
+
+    factorial_n = factorial(n);
+    factorial_neg_n = factorial(n);
+
+    cout << n << "!! = " << factorial_n << endl;
+    cout << "(-1)^" << n << "+1" << " * " << n << "!! = " << (-1) * factorial_neg_n << endl;
+
+    return 0;
+}
---
+++ lab2_F2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+T factorial(T n) {
+    T result = 1;
+    for (T j = 1; j <= n; ++j) {
+        result *= j;
+    }
+    return result;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "rus");
+    int n;
+    long long factorial_n, factorial_neg_n;
+
+    cout << "Введіть n: ";
+    cin >> n;
+
+    factorial_n = factorial(n);
+    factorial_neg_n = factorial(n);
+
+    cout << n << "!! = " << factorial_n << endl;
+    cout << "(-1)^" << n << "+1" << " * " << n << "!! = " << (-1) * factorial_neg_n << endl;
+
+    return 0;
+}
